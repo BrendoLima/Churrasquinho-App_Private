69bea6c576777acffe5580d13a389001a727a721
BrendoLima <47870425+BrendoLima@users.noreply.github.com>
2021-10-28 16:46:24 -0300

    ok

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,7 +1,21 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
+
+import { doc, getDoc } from "firebase/firestore";
 
 export default function HomePage() {
+  /* 
+  const  (db, "produtos", "SF");
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    console.log("Document data:", docSnap.data());
+  } else {
+    // doc.data() will be undefined in this case
+    console.log("No such document!");
+  }
+  */
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Você está na HomePage</Text>

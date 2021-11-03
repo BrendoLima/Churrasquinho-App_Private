ed05a8f759f0b0b01a53aa29db9d92f43216a403
BrendoLima <47870425+BrendoLima@users.noreply.github.com>
2021-11-03 15:57:24 -0300

    ok

--- Pages/Comanda.jsx
+++ Pages/Comanda.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { View } from "react-native";
+import firestore from "../database/firebase";
 
 export default function Comanda() {
-
+  function database() {
+    const ref = firestore().collection("produtos");
+    console.log(ref);
+  }
+  database();
   return (
     <View>
       <h1>ola</h1>
